79dd3eab260fee274922c0954c9450b4402f6db8
Vikram S <45453408+vikram1155@users.noreply.github.com>
2021-02-17 22:54:34 +0530

    Update NotFound.js

--- src/NotFound.js
+++ src/NotFound.js
@@ -7,7 +7,7 @@ const NotFound=()=> {
         <div>
             <h1>Find all the blogs in Home page</h1><br></br>
             <div className="Notfound">
-            <Link to='/home'>Back to home</Link>
+            <Link to='/CRUD-APP/home'>Back to home</Link>
             </div>
         </div>
     )
